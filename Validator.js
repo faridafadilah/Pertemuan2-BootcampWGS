f60aabe5232bbaf8db9d8a6bba23cfa88ddb0461
Farida Fadilah <faridafadilah42807@gmail.com>
2022-09-07 10:23:51 +0700

    Belajar Validasi

--- Validator.js
+++ Validator.js
@@ -5,5 +5,5 @@ console.log(validator.isEmail('farida@gmail.com')); //true
 console.log(validator.isEmail('farida@gmail.m')); //false
 
 // Validasi Mobile Phone
-console.log(validator.isMobilePhone('08815877610', 'id-ID')); //true
+console.log(validator.isMobilePhone('08875877610', 'id-ID')); //true
 console.log(validator.isMobilePhone('033215877610', 'id-ID')); //true
